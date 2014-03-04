2fb63363ba72fd7293eab6dd736fbe1eddd87f4a
Poppy DELL <poppy-dell@flowers.com>
2014-03-04 17:20:54 +0100

    Dernier fillage

--- poppytools/configuration/config.py
+++ poppytools/configuration/config.py
@@ -4,3 +4,5 @@ import json
 with open(os.path.join(os.path.dirname(__file__), 'poppy_config.json'),'r') as f:
     poppy_config = json.load(f)
 
+with open(os.path.join(os.path.dirname(__file__), 'poppy_config2.json'),'r') as f:
+    poppy_config2 = json.load(f)
