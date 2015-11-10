b0287aa15287802f76fa9240b1e2c0b68ebc40d4
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2015-11-10 16:31:30 +0100

    delete everything from the master branch so no one ever will build the beta instead of the current version by mistake. All files are still present on the deprecated branch and in the history of the master one...

--- software/poppytools/configuration/config.py
+++
@@ -1,5 +0,0 @@
-import os
-import json
-
-with open(os.path.join(os.path.dirname(__file__), 'poppy_config.json'),'r') as f:
-    poppy_config = json.load(f)
--- software/poppytools/utils/conversion.py
+++
@@ -1,8 +0,0 @@
-import json
-import numpy
-
-class NumpyAwareJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, numpy.ndarray) and obj.ndim == 1:
-                return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
